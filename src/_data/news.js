3b477246b46130b05615a6e8b2c49f909763eae8
EngrZaks <abzakariyya@gmail.com.com>
2020-08-30 00:21:21 +0100

    descriptive api key

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = async function () {
      try {
           const response = await axios.get(
-               `https://newsapi.org/v2/top-headlines?country=ng&apiKey=${process.env.API_KEY}&pageSize=10`
+               `https://newsapi.org/v2/top-headlines?country=ng&apiKey=${process.env.NEWS_API_KEY}&pageSize=10`
           );
           return response.data;
      } catch (error) {
